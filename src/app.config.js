08e703ffb487dc77b37e2e3c2dd6d59d1963f0e5
lianggang <liangganglovelife@gmail.com>
2021-07-13 13:52:16 +0800

    提交图片

--- src/app.config.js
+++ src/app.config.js
@@ -4,8 +4,39 @@ export default {
   ],
   window: {
     backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
+    navigationBarBackgroundColor: '#f6f6f6',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    selectedColor: '#818181',
+    backgroundColor: '#ffffff',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: './assets/images/tabBar/home_0325.png',
+        selectedIconPath: './assets/images/tabBar/tab_home_select1119.png',
+      },
+      {
+        pagePath: 'pages/index/index',
+        text: '看资产',
+        iconPath: './assets/images/tabBar/tab_touzi.png',
+        selectedIconPath: './assets/images/tabBar/tab_touzi_select.png',
+      },
+      {
+        pagePath: 'pages/index/index',
+        text: '找资金',
+        iconPath: './assets/images/tabBar/tab_rongzi.png',
+        selectedIconPath: './assets/images/tabBar/tab_rongzi_select.png',
+      },
+      {
+        pagePath: 'pages/index/index',
+        text: '我的',
+        iconPath: './assets/images/tabBar/tab_my1217.png',
+        selectedIconPath: './assets/images/tabBar/tab_my_select1217.png',
+      },
+    ],
+  },
 }
